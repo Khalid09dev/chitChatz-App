4065da9e07ebc6ceea33e8983b7a8c3cf09d94e1
khalid09dev <mdkhalidhossen@hotmail.com>
2024-08-05 18:01:12 +0600

    added a search form in messages page

--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -1,8 +1,19 @@
+import { useRef } from "react";
+import { IoSearchOutline } from "react-icons/io5";
 
 const Messages = () => {
+    const inputRef = useRef(null);
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(inputRef.current.value);
+    }
     return (
         <div>
-            <h2>Messages</h2>
+            <form className="flex relative pt-16 ml-7" onSubmit={handleSubmit}>
+                <input className="pl-12 py-3 w-72 rounded-xl placeholder:text-[#7C7C7C] placeholder:text-xl outline-none shadow-md shadow-[#79c5ef61]" ref={inputRef} type="text" placeholder="Search"/>
+                <IoSearchOutline className="absolute left-4 text-2xl mt-3"/>
+            </form>
         </div>
     );
 };
