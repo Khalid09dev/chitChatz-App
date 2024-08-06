6d9d24317ce1bf0ba3383c2e9a05ebd242cfd6b8
khalid09dev <mdkhalidhossen@hotmail.com>
2024-08-06 16:37:26 +0600

    added directOrder component of landing page

---
+++ src/components/DirectOrdersLandingPage/DirectOrders.jsx
@@ -0,0 +1,11 @@
+import directOrder from '../../../public/get_direct_orders.svg';
+
+const DirectOrders = () => {
+    return (
+        <div className='bg-[#F8F8FA] py-20'>
+            <img className='mx-auto' src={directOrder} alt="#" />
+        </div>
+    );
+};
+
+export default DirectOrders;
--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,3 +1,4 @@
+import DirectOrders from "../../components/DirectOrdersLandingPage/DirectOrders";
 import FeaturesLandingPage from "../../components/FeaturesLandingPage/FeaturesLandingPage";
 import Landing_Banner from "../../components/Landing_Banner/Landing_Banner";
 import Landing_Navbar from "../../components/Landing_Navbar/Landing_Navbar";
@@ -12,6 +13,7 @@ const LandingPage = () => {
             <FeaturesLandingPage></FeaturesLandingPage>
             <LiveVideoChat></LiveVideoChat>
             <SellingConversation></SellingConversation>
+            <DirectOrders></DirectOrders>
         </div>
     );
 };
