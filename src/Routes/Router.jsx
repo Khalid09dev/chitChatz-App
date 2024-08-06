574cc168a47deb9c6bc0cedecd4f330890f3e677
khalid09dev <mdkhalidhossen@hotmail.com>
2024-08-06 22:23:25 +0600

    added a simple signup form

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -3,6 +3,7 @@ import App from "../App";
 import Messages from '../components/Messages/Messages';
 import LandingPage from '../pages/LandingPage/LandingPage';
 import Login from '../pages/Login/Login';
+import Signup from "../pages/Signup/Signup";
 
 const Router = createBrowserRouter([
     {
@@ -12,6 +13,10 @@ const Router = createBrowserRouter([
     {
         path: '/login',
         element: <Login></Login>
+    },
+    {
+        path: '/signup',
+        element: <Signup></Signup>
     },
     {
         path: '/app',
--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -4,19 +4,19 @@ import { FaFacebook } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 
-const Login = () => {
+const Signup = () => {
     return (
         <div className="flex items-center justify-center h-screen bg-gradient-to-r from-blue-500 to-blue-900">
             <div className="w-2/6 border border-[#5882c17d] py-10 px-20 bg-[#5882c147] backdrop-blur-2xl rounded-lg">
                 <h2 className="text-[#FB8E0B] text-3xl font-semibold text-center pb-8">ChitChatz</h2>
-                <span className="text-white text-2xl">Login</span>
+                <span className="text-white text-2xl">Signup</span>
                 <form className="pt-4">
                     <label className="text-white">Email</label> <br />
                     <input className="w-full py-2 pl-3 rounded-md mb-4" type="text" placeholder="username@gmail.com"/> <br />
                     <label className="text-white">Password</label> <br />
                     <input className="w-full py-2 pl-3 rounded-md mb-1" type="password" name="" id="" placeholder="Password"/> <br />
                     <span className="text-white">Forget Password?</span> <br />
-                    <input className="text-white bg-[#003465] w-full text-base py-2 rounded-lg mt-7" type="submit" value="Sign in" />
+                    <input className="text-white bg-[#003465] w-full text-base py-2 rounded-lg mt-7" type="submit" value="Sign up" />
                 </form>
                     <p className="text-white text-center py-5">or continue with</p>
                     <div className="flex justify-center gap-4">
@@ -24,10 +24,10 @@ const Login = () => {
                         <FaGithub className="p-1 w-16 bg-white text-[35px] rounded-md"/>
                         <FaFacebook className="p-1 w-16 bg-white text-blue-500 text-[35px] rounded-md"/>
                     </div>
-                    <p className="text-white pt-5 text-center">Dont have an account yet?<Link to="/signup" className="pl-1">Register for free</Link></p>
+                    <p className="text-white pt-5 text-center">Already have an account?<Link to="/login" className="pl-1">please login</Link></p>
             </div>
         </div>
     );
 };
 
-export default Login;
+export default Signup;
