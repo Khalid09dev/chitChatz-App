e03407af5258a996f1b75b921ec17711bbddc3cb
khalid09dev <mdkhalidhossen@hotmail.com>
2024-08-07 13:38:06 +0600

    added password visibility toggle functionality in signup page

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -2,9 +2,12 @@ import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
 import { FaFacebook } from "react-icons/fa";
 import { Link, useNavigate } from "react-router-dom";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "../../providers/AuthProvider";
 import Swal from 'sweetalert2';
+import { FaRegEyeSlash } from "react-icons/fa6";
+import { FaRegEye } from "react-icons/fa6";
+
 
 const Signup = () => {
     const {createUser, googleSignIn, logOut} = useContext(AuthContext);
@@ -55,6 +58,13 @@ const Signup = () => {
             console.log(error);
         })
     }
+
+    //handle password visibility toggle
+    const [visible, setVisible] = useState(false);
+    const handlePassToggle = () => {
+        setVisible(!visible);
+        // console.log('password visibility toggle', visible);
+    }
     return (
         <div className="flex items-center justify-center h-screen bg-gradient-to-r from-blue-500 to-blue-900">
             <div className="w-2/6 border border-[#5882c17d] py-10 px-20 bg-[#5882c147] backdrop-blur-2xl rounded-lg">
@@ -64,7 +74,9 @@ const Signup = () => {
                     <label className="text-white">Email</label> <br />
                     <input className="w-full py-2 pl-3 rounded-md mb-4" type="email" name="email" placeholder="username@gmail.com"/> <br />
                     <label className="text-white">Password</label> <br />
-                    <input className="w-full py-2 pl-3 rounded-md mb-1" type="password" name="password" id="" placeholder="Password"/> <br />
+                    <div className="relative">
+                        <input className="w-full py-2 pl-3 rounded-md mb-1" type={visible ? 'text' : 'password'} name="password" id="" placeholder="Password"/><div className="absolute bottom-3.5 right-4 cursor-pointer text-xl" onClick={handlePassToggle}>{visible ? <FaRegEyeSlash/> : <FaRegEye/>}</div>
+                    </div><br />
                     <span className="text-white">Forget Password?</span> <br />
                     <input className="text-white bg-[#003465] w-full text-base py-2 rounded-lg mt-7" type="submit" value="Sign up" />
                 </form>
