de446bcfd648e5cefb45bbf158a6e83d243d39cd
khalid09dev <mdkhalidhossen@hotmail.com>
2024-08-13 20:50:36 +0600

    added proxy url for fixing cors issue

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'https://chit-chatz-server.vercel.app'
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      }
     }
   }
 })
