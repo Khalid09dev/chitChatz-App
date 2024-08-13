afe1ed9aceb5e36efc7787c8bb7539669bdc523e
khalid09dev <mdkhalidhossen@hotmail.com>
2024-08-13 21:39:57 +0600

    wrap the application with AuthContextProvider and SocketContextProvider

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,19 @@ import App from './App.jsx';
 import { RouterProvider } from 'react-router-dom';
 import router from './Routes/Router.jsx';
 import AuthProvider from './providers/AuthProvider.jsx';
+import { AuthContextProvider } from './context/AuthContext.jsx';
+import { SocketContextProvider } from './context/SocketContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
-      <RouterProvider router={router}>
-        <App />
-      </RouterProvider>
+      <AuthContextProvider>
+        <SocketContextProvider>
+          <RouterProvider router={router}>
+            <App />
+          </RouterProvider>
+        </SocketContextProvider>
+      </AuthContextProvider>
     </AuthProvider>
   </React.StrictMode>,
 )
